8649129bf451e8266c51d2a58203373d68167909
awsandy <awsandy@amazon.co.uk>
2020-05-21 19:29:49 +0100

    first commit

---
+++ k8s2tf.sh
@@ -0,0 +1,94 @@
+mycluster="ateks1"
+f="no"
+mkdir -p generated/tf.$mycluster
+cd generated/tf.$mycluster
+if [ "$f" = "no" ]; then
+    echo "Cleaning generated/tf.$mycluster"
+    rm -f import.log resources*.txt
+    rm -f processed.txt
+    rm -f *.tf *.json
+    rm -f terraform.*
+    rm -rf .terraform
+else
+    sort -u processed.txt > pt.txt
+    cp pt.txt processed.txt
+fi
+# write the k8s.tf file
+cd generated/tf.$mycluster
+printf "provider \"kubernetes\" {}\n" > k8s.tf
+pwd
+
+
+
+terraform init
+
+pre="*"
+t="*"
+
+date
+pwd
+lc=0
+
+echo "t=$t"
+echo "loop through providers"
+pwd
+for com in `ls ../../scripts/$pre-*$t*.sh | cut -d'/' -f4 | sort -g`; do    
+        echo "$com"
+        docomm=". ../../scripts/$com $i"
+        if [ "$f" = "no" ]; then
+            eval $docomm 2>&1 | tee -a import.log
+        else
+            grep "$docomm" processed.txt
+            if [ $? -eq 0 ]; then
+                echo "skipping $docomm"
+            else
+                eval $docomm 2>&1 | tee -a import.log
+            fi
+        fi
+        lc=`expr $lc + 1`
+
+        file="import.log"
+        while IFS= read -r line
+        do
+            if [[ "${line}" == *"Error"* ]];then
+          
+                if [[ "${line}" == *"Duplicate"* ]];then
+                    echo "Ignoring $line"
+                else
+                    echo "Found Error: $line exiting .... (pass for now)"
+                    pass
+                fi
+            fi
+
+        done <"$file"
+
+        echo "$docomm" >> processed.txt
+        
+    
+    rm -f terraform*.backup
+done
+
+#########################################################################
+
+
+date
+
+
+echo "---------------------------------------------------------------------------"
+echo "aws2tf output files are in generated/tf.$mycluster"
+echo "---------------------------------------------------------------------------"
+
+echo "Terraform fmt ..."
+terraform fmt
+echo "Terraform validate ..."
+terraform validate .
+
+if [ "$v" = "yes" ]; then
+    exit
+fi
+
+echo "Terraform Plan ..."
+terraform plan .
+
+
+echo "code in generated/tf.$mycluster"
---
+++ scripts/010-namespaces.sh
@@ -0,0 +1,47 @@
+pwd
+ttft="kubernetes_namespace"
+kmaps=`kubectl get namespaces -o json | jq .items[].metadata.name`
+for i in $kmaps; do
+    echo $i
+    cname=`echo $i | tr -d '"'`
+    printf "resource \"%s\" \"%s\" {" $ttft $cname > $ttft.$cname.tf
+    printf "}" $cname >> $ttft.$cname.tf
+    terraform import $ttft.$cname $cname
+    terraform state show $ttft.$cname > t2.txt
+    rm $ttft.$cname.tf
+    cat t2.txt | perl -pe 's/\x1b.*?[mGKH]//g' > t1.txt
+            #	for k in `cat t1.txt`; do
+            #		echo $k
+            #	done
+    file="t1.txt"
+    fn=`printf "%s__%s.tf" $ttft $cname`
+
+            while IFS= read line
+            do
+				skip=0
+                # display $line or do something with $line
+                t1=`echo "$line"` 
+                if [[ ${t1} == *"="* ]];then
+                    tt1=`echo "$line" | cut -f1 -d'=' | tr -d ' '` 
+                    tt2=`echo "$line" | cut -f2- -d'='`
+                    if [[ ${tt1} == "id" ]];then skip=1; fi  
+                    if [[ ${tt1} == "self_link" ]];then skip=1; fi
+                    if [[ ${tt1} == "uid" ]];then skip=1; fi 
+                    if [[ ${tt1} == "resource_version" ]];then skip=1; fi             
+                    if [[ ${tt1} == "generation" ]];then skip=1; fi 
+                    if [[ ${tt1} == "vpc_id" ]]; then
+                        tt2=`echo $tt2 | tr -d '"'`
+                        t1=`printf "%s = aws_vpc.%s.id" $tt1 $tt2`
+                    fi
+
+                fi
+                if [ "$skip" == "0" ]; then
+                    #echo $skip $t1
+                    echo $t1 >> $fn
+                fi
+                
+            done <"$file"
+            sed -i .bak 's/<<~/<</g' $fn
+            rm -f *.tf.bak
+done
+terraform fmt
